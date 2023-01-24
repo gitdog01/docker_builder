f4b02f82f1bb081a88db07cb29e10f5cbf7a5d9f
gitdog01 <hostdog01@naver.com>
2023-01-24 19:15:41 +0900

    docker ps

--- main.go
+++ main.go
@@ -1,10 +1,31 @@
 package main
 
 import (
+	"context"
 	"fmt"
+
+	"github.com/docker/docker/api/types"
+	"github.com/docker/docker/client"
 )
 
+func dockerConnect() {
+	fmt.Println("Docker Connect")
+	cli, err := client.NewClientWithOpts(client.FromEnv)
+	if err != nil {
+		panic(err)
+	}
+
+	containers, err := cli.ContainerList(context.Background(), types.ContainerListOptions{})
+	if err != nil {
+		panic(err)
+	}
+
+	for _, container := range containers {
+		fmt.Printf("%s %s\n", container.ID[:10], container.Image)
+	}
+}
 
 func main()  {
 	fmt.Println("Hello World")
+	dockerConnect();
 }
